71eaa3552b206344770ac901eaae1e846a8af95a
phyng <phyngk@gmail.com>
2025-01-14 23:24:28 +0800

    feat(scilib): add gpt module and gpt_trans

--- scilib/gpt/gpt_trans.py
+++ scilib/gpt/gpt_trans.py
@@ -1,34 +1,36 @@
 # coding: utf-8
 
 from __future__ import unicode_literals, absolute_import, print_function, division
-
 from typing import Optional
-
 from scilib.gpt.gpt_api import call_gpt
 
 
-def detect_gender(
+def gpt_trans(
     name: str,
+    to_languege: str,
     *,
     system_prompt: Optional[str] = None,
     api_key: Optional[str] = None,
     api_url: Optional[str] = None
 ) -> Optional[str]:
-    """根据提示词识别性别
+    """根据提示词翻译
     Args:
         name: 待识别的名字
         system_prompt: 系统提示词
         api_key: API密钥，如果为None则从环境变量获取
         api_url: API地址，如果为None则从环境变量获取
-    Returns:
-        'male' 或 'female' 或 'unknown' 或 'error'
     """
-    system_prompt = system_prompt or """你是一个性别识别助手。请分析输入姓名中的性别。
-    只需返回 'male' 或 'female'，如果无法判断则返回 'unknown'。不需要其他解释。"""
+    system_prompt = system_prompt or """You are a professional, authentic machine translation engine"""
 
     messages = [
-        {"role": "system", "content": system_prompt},
-        {"role": "user", "content": name}
+        {
+            "role": "system",
+            "content": system_prompt
+        },
+        {
+            "role": "user",
+            "content": f"""Translate input into {to_languege}, output translation ONLY. NO explanations. NO notes. Input: {name}"""  # noqa
+        }
     ]
 
     result = call_gpt(
@@ -37,14 +39,10 @@ def detect_gender(
         api_key=api_key,
         api_url=api_url
     )
-    if result:
-        result = result.lower().strip()
-        if result in ['male', 'female', 'unknown']:
-            return result
-    return 'error'
+    return result
 
 
 # 使用示例
 if __name__ == '__main__':
-    for text in ["李彩霞", "王大雷", "江铜"]:
-        print(f'文字：{text} 检测到的性别: {detect_gender(text)}')
+    for text in ["hello, world"]:
+        print(f"{text} 翻译为: {gpt_trans(text, '中文')}")
