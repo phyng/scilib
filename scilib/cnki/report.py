0816d0003f4efe6a008e45c30439a6c3be0b078f
phyng <phyngk@gmail.com>
2025-06-01 10:57:25 +0800

    feat(cnki): update cnki report

--- scilib/cnki/report.py
+++ scilib/cnki/report.py
@@ -5,6 +5,8 @@ from collections import Counter
 import os
 from pathlib import Path
 import pandas as pd
+from scilib.cnki.parser import get_clc_map
+from scilib.iolib.file import write_file
 
 from scilib.corrs.corrs_utils import (
     get_corrs,
@@ -19,7 +21,21 @@ ORG_TABLE_PATH = Path(__file__).parent / 'config/org_table.xlsx'
 ORG_MATCH_RULE_PATH = Path(__file__).parent / 'config/org_match_rule.xlsx'
 
 
-def report_cnki_org(cnki_items, *, outpur_dir):
+def export_tokens_list(tokens_list, prefix, outpur_dir, top_size):
+    tokens_list = ([i for i in li if i] for li in tokens_list if li)
+    tokens_list = [li for li in tokens_list if li]
+    counter, corrs = get_corrs(tokens_list, top_size=top_size)
+    write_file(os.path.join(outpur_dir, f"{prefix}.corrs.csv"), corrs_to_csv_string(corrs))
+    counter_items = []
+    for i, (k, v) in enumerate(counter.most_common()):
+        counter_items.append(dict(i=i + 1, name=k, count=v))
+    pd.DataFrame.from_records(counter_items).to_csv(
+        os.path.join(outpur_dir, f"{prefix}.items.csv"), index=False
+    )
+
+
+def report_cnki_org(cnki_items, *, outpur_dir, top_size=50):
+    clc_map = get_clc_map()
     df_org_table = pd.read_excel(ORG_TABLE_PATH)
     df_org_match_rule = pd.read_excel(ORG_MATCH_RULE_PATH)
 
@@ -27,15 +43,41 @@ def report_cnki_org(cnki_items, *, outpur_dir):
     org_table_names = sorted(org_table_names, key=lambda x: (-len(x), x))
     org_match_rules = {row['规则'].strip(): row['目标'].strip() for i, row in df_org_match_rule.iterrows()}
 
+    # author
     authors_list = [
         [i.strip() for i in token.split(';') if i and i.strip()]
         for token in [i.get('Author', '') for i in cnki_items if i]
         if token
     ]
-    counter = Counter([i for li in authors_list for i in li])
-    author_items = []
-    for i, (k, v) in enumerate(counter.most_common()):
-        author_items.append(dict(i=i + 1, name=k, count=v))
+    export_tokens_list(authors_list, 'author', outpur_dir, top_size)
+
+    # FirstDuty
+    first_duty_list = [
+        [i.strip() for i in token.split(';') if i and i.strip()]
+        for token in [i.get('FirstDuty', '') for i in cnki_items if i]
+        if token
+    ]
+    export_tokens_list(first_duty_list, 'first_duty', outpur_dir, top_size)
+
+    # clc_level1_tokens
+    export_tokens_list(
+        [li for li in [i.get('clc_level1_tokens', []) for i in cnki_items] if li],
+        'clc_level1', outpur_dir, top_size
+    )
+    export_tokens_list(
+        [[clc_map.get(i, i) for i in li] for li in [i.get('clc_level1_tokens', []) for i in cnki_items] if li],
+        'clc_level1_name', outpur_dir, top_size
+    )
+
+    # clc_level2_tokens
+    export_tokens_list(
+        [li for li in [i.get('clc_level2_tokens', []) for i in cnki_items] if li],
+        'clc_level2', outpur_dir, top_size
+    )
+    export_tokens_list(
+        [[clc_map.get(i, i) for i in li] for li in [i.get('clc_level2_tokens', []) for i in cnki_items] if li],
+        'clc_level2_name', outpur_dir, top_size
+    )
 
     orgs_list = [
         [i.strip() for i in token.split(';') if i.strip()]
@@ -68,19 +110,19 @@ def report_cnki_org(cnki_items, *, outpur_dir):
                     match_name = n
                     match_type = 'contains'
                     break
+
         item['match_name'] = match_name
         item['match_type'] = match_type
+
         if match_name:
             org_map[item['name']] = match_name
+        else:
+            org_map[item['name']] = item['name']
 
     org_items_matched = [i for i in org_items if i['name'] in org_map]
     pd.DataFrame.from_records(org_items).to_csv(os.path.join(outpur_dir, "org.items.csv"), index=False)
     pd.DataFrame.from_records(org_items_matched).to_csv(
         os.path.join(outpur_dir, "org.items_matched.csv"), index=False
-    )
-
-    pd.DataFrame.from_records(author_items).to_csv(
-        os.path.join(outpur_dir, "author.items.csv"), index=False
     )
 
     matched_orgs_list = []
@@ -90,17 +132,16 @@ def report_cnki_org(cnki_items, *, outpur_dir):
             if org in org_map and org_map[org] not in matched_orgs:
                 matched_orgs.append(org_map[org])
         matched_orgs_list.append(matched_orgs)
-    _, corrs = get_corrs(matched_orgs_list)
-    corrs_csv_string = corrs_to_csv_string(corrs)
-    with open(os.path.join(outpur_dir, "org.corrs.csv"), "w") as f:
-        f.write(corrs_csv_string)
+
+    # corrs
+    _, corrs = get_corrs(matched_orgs_list, top_size=top_size)
+    write_file(os.path.join(outpur_dir, "org.corrs.csv"), corrs_to_csv_string(corrs))
 
     # pnetview txt format
     pnetview_text = '\n'.join(
         [','.join(uniqify([t.replace(',', '-') for t in orgs])) for orgs in matched_orgs_list if orgs]
     )
-    with open(os.path.join(outpur_dir, "org.pnetview.txt"), "w") as f:
-        f.write(pnetview_text)
+    write_file(os.path.join(outpur_dir, "org.pnetview.txt"), pnetview_text)
 
 
 def report_cnki_keywords(
@@ -112,9 +153,7 @@ def report_cnki_keywords(
     counter, corrs = get_corrs([item["keyword_tokens"] for item in cnki_items], top_size=top_size)
     top_n = [token for token, _ in counter.most_common(top_size)]
 
-    corrs_csv_string = corrs_to_csv_string(corrs)
-    with open(os.path.join(outpur_dir, "keywords.corrs.csv"), "w") as f:
-        f.write(corrs_csv_string)
+    write_file(os.path.join(outpur_dir, "keywords.corrs.csv"), corrs_to_csv_string(corrs))
 
     keywords_items = []
     for i, (k, v) in enumerate(counter.most_common()):
@@ -125,8 +164,7 @@ def report_cnki_keywords(
 
     # cortext network
     cortext_network = corrs_to_cortext_network(corrs)
-    with open(os.path.join(outpur_dir, "keywords.cortext.csv"), "w") as f:
-        f.write(cortext_network_to_csv_string(cortext_network))
+    write_file(os.path.join(outpur_dir, "keywords.cortext.csv"), cortext_network_to_csv_string(cortext_network))
 
     # keywords_year_extend
     top_keywords_year_extend = []
@@ -142,8 +180,10 @@ def report_cnki_keywords(
                 top_keywords_year_extend.extend(dict(year=year, keyword=k) for i in range(v))
 
     year_cortext_networks = merge_year_cortext_networks(networks)
-    with open(os.path.join(outpur_dir, "keywords.cortext_with_year.csv"), "w") as f:
-        f.write(cortext_network_to_csv_string(year_cortext_networks))
+    write_file(
+        os.path.join(outpur_dir, "keywords.cortext_with_year.csv"),
+        cortext_network_to_csv_string(year_cortext_networks)
+    )
 
     pd.DataFrame.from_records(top_keywords_year_extend).to_csv(
         os.path.join(outpur_dir, "keywords.top.year_extend.csv"), index=False
@@ -153,8 +193,7 @@ def report_cnki_keywords(
     pnetview_text = '\n'.join(
         [','.join(uniqify([t.replace(',', '-') for t in item["keyword_tokens"]])) for item in cnki_items]
     )
-    with open(os.path.join(outpur_dir, "keywords.pnetview.txt"), "w") as f:
-        f.write(pnetview_text)
+    write_file(os.path.join(outpur_dir, "keywords.pnetview.txt"), pnetview_text)
 
 
 def report_cnki_all(
@@ -169,4 +208,4 @@ def report_cnki_all(
         outpur_dir=outpur_dir,
         top_size=keywords_top_size
     )
-    report_cnki_org(cnki_items, outpur_dir=outpur_dir)
+    report_cnki_org(cnki_items, outpur_dir=outpur_dir, top_size=keywords_top_size)
